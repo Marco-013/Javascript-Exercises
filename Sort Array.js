719210e3dcfaf2565568421c82ae0663d3dcaabc
Marco <johnmarco.silvestre013@gmail.com>
2026-02-08 17:48:18 +0800

    Sort Array

---
+++ Sort Array.js
@@ -0,0 +1,20 @@
+const array = [10, [4], [7], 2, 5];
+
+function sortArray(arr){
+  
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+
+      if (arr[j] > arr[j + 1]) {
+        
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+console.log(sortArray(array));
